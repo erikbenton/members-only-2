59f753a2dd15ee242dab0ad3c76d13ddf833dc46
erikbenton <erik.benton@hotmail.com>
2018-02-06 14:25:12 -0500

    adding remember token to users when logging in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
+	attr_accessor :remember_token
+
+	before_create :remember
+
 	# Validating user attributes
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -9,5 +13,18 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+	def new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def digest(string)
+		Digest::SHA1.hexdigest(string)
+	end
+
+	private
+		def remember
+			self.remember_token = User.new_token
+			self.remember_digest = User.digest(remember_token)
+		end
 	
 end
